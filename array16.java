e7ee2d2fce9772d5d979843fe65df64bed9fb271
KrishnaAsati16 <krishnaasati2006@gmail.com>
2025-12-06 12:26:18 +0530

    Missing number in array [1 to n]

---
+++ array16.java
@@ -0,0 +1,17 @@
+public class array16 {
+    public static void main(String[] args) {
+         int nums[] = {3,0,1,};
+         System.out.println( MissingNum(nums));
+    }
+
+    public static int MissingNum(int nums[]) {
+        int sum =0;
+        for(int i = 0; i<nums.length;i++){
+          sum = sum +nums[i];
+        }
+        int actualSum = nums.length * (nums.length + 1) / 2;
+
+        int missingNum = actualSum - sum;
+        return missingNum;
+    }
+}
